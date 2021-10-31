db14b8fb7a504d8734dabeb5540a9696f4b4dc2d
Milad Roghanian <miladr100@gmail.com>
2021-10-30 22:32:53 -0400

    feat: cria estrutura de sobre

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,24 @@
 <template>
   <v-row justify="center" class="mt-4">
-    <v-col cols="12" sm="8" md="5">
-      <v-card class="logo py-4 d-flex justify-center">
-        <v-card-title>
-          SOBRE
+    <v-col cols="12" sm="10" md="10">
+      <v-card class="d-flex flex-column align-center">
+        <v-card-title class="text-h3 font-weight-bold">
+          Nossa História
         </v-card-title>
+
+        <v-row>
+          <v-col cols="6">
+            <v-card-text class="text-h6 text--primary font-weight-regular text-justify">
+              A sensibilização e a organização de cadeias para exportação começaram em 2016, com os tradicionais seminários da Rede InterAgro. Como resultado positivo, os empresários rurais passaram a solicitar ações mais estruturadas e continuadas de apoio a exportações. Também se percebeu que estes encontravam dificuldades para comercialização no exterior.
+            </v-card-text>
+          </v-col>
+
+          <v-col cols="6">
+            <v-card-text class="text-h6 text--primary font-weight-regular text-justify">
+              A sensibilização e a organização de cadeias para exportação começaram em 2016, com os tradicionais seminários da Rede InterAgro. Como resultado positivo, os empresários rurais passaram a solicitar ações mais estruturadas e continuadas de apoio a exportações. Também se percebeu que estes encontravam dificuldades para comercialização no exterior.
+            </v-card-text>
+          </v-col>
+        </v-row>
       </v-card>
     </v-col>
   </v-row>
