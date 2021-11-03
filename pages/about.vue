bb8f32196103db8bd32f08eb0e92535b352338b3
Milad Roghanian <miladr100@gmail.com>
2021-11-02 23:34:32 -0400

    feat: add pag about e contact

--- pages/about.vue
+++ pages/about.vue
@@ -1,25 +1,146 @@
 <template>
   <v-row justify="center" class="mt-4">
-    <v-col cols="12" sm="10" md="10">
-      <v-card class="d-flex flex-column align-center">
-        <v-card-title class="text-h3 font-weight-bold">
-          Nossa História
-        </v-card-title>
+    <v-row justify="center" class="mt-4">
+      <v-col cols="12" sm="10" md="10">
+        <v-card class="d-flex flex-column align-center">
+          <v-card-title class="text-h3 font-weight-bold">
+            Nossa História
+          </v-card-title>
 
-        <v-row>
-          <v-col cols="6">
-            <v-card-text class="text-h6 text--primary font-weight-regular text-justify">
-              A sensibilização e a organização de cadeias para exportação começaram em 2016, com os tradicionais seminários da Rede InterAgro. Como resultado positivo, os empresários rurais passaram a solicitar ações mais estruturadas e continuadas de apoio a exportações. Também se percebeu que estes encontravam dificuldades para comercialização no exterior.
-            </v-card-text>
-          </v-col>
+          <v-row>
+            <v-col cols="6">
+              <v-card-text class="text-h6 text--primary font-weight-regular text-justify">
+                A sensibilização e a organização de cadeias para exportação começaram em 2016, com os tradicionais seminários da Rede InterAgro. Como resultado positivo, os empresários rurais passaram a solicitar ações mais estruturadas e continuadas de apoio a exportações. Também se percebeu que estes encontravam dificuldades para comercialização no exterior.
+              </v-card-text>
+            </v-col>
 
-          <v-col cols="6">
-            <v-card-text class="text-h6 text--primary font-weight-regular text-justify">
-              A sensibilização e a organização de cadeias para exportação começaram em 2016, com os tradicionais seminários da Rede InterAgro. Como resultado positivo, os empresários rurais passaram a solicitar ações mais estruturadas e continuadas de apoio a exportações. Também se percebeu que estes encontravam dificuldades para comercialização no exterior.
-            </v-card-text>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-col>
+            <v-col cols="6">
+              <v-card-text class="text-h6 text--primary font-weight-regular text-justify">
+                A sensibilização e a organização de cadeias para exportação começaram em 2016, com os tradicionais seminários da Rede InterAgro. Como resultado positivo, os empresários rurais passaram a solicitar ações mais estruturadas e continuadas de apoio a exportações. Também se percebeu que estes encontravam dificuldades para comercialização no exterior.
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center" align="center" class="mt-12">
+      <v-card-title class="text-h4 font-weight-bold">
+        Essas são algumas de nossas negociações
+      </v-card-title>
+    </v-row>
+
+    <v-row v-for="item in items" :key="item.title" justify="center" class="mt-4">
+      <v-col cols="12" sm="10" md="10">
+        <v-card>
+          <v-row justify="center" align="center" class="mt-4">
+            <v-col v-if="!item.rightDirection" cols="7">
+              <v-card-title class="text-h4 font-weight-bold">
+                {{item.title}}
+              </v-card-title>
+              <v-card-text v-for="(text,ind) in item.texts" :key="ind" class="text-h7 text-justify">   
+                {{text}}
+              </v-card-text>
+            </v-col>
+            <v-col cols="5">
+              <v-container>
+                <v-carousel
+                  :cycle="true"
+                  :show-arrows="false"
+                  hide-delimiter-background
+                  delimiter-icon="mdi-minus"
+                  height="380"
+                  width="200"
+                >
+                  <v-carousel-item
+                    v-for="(carrouselItem,index) in item.carrousel"
+                    :key="index"
+                    :src="carrouselItem.src"
+                    reverse-transition="fade-transition"
+                    transition="fade-transition"
+                  ></v-carousel-item>
+                </v-carousel>
+              </v-container>
+            </v-col>
+            <v-col v-if="item.rightDirection" cols="7">
+              <v-card-title class="text-h4 font-weight-bold">
+                {{item.title}}
+              </v-card-title>
+              <v-card-text v-for="(text,ind) in item.texts" :key="ind" class="text-h7 text-justify">   
+                {{text}}
+              </v-card-text>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-row>
 </template>
+
+<script>
+import imagCarrousel01 from '~/static/img/index_carrousel/index_carrousel_01.jpeg';
+import imagCarrousel02 from '~/static/img/index_carrousel/index_carrousel_02.jpeg';
+import imagCarrousel03 from '~/static/img/index_carrousel/index_carrousel_03.jpeg';
+import imagCarrousel04 from '~/static/img/index_carrousel/index_carrousel_04.jpeg';
+import imagCarrousel05 from '~/static/img/index_carrousel/index_carrousel_05.jpeg';
+import imagCarrousel06 from '~/static/img/index_carrousel/index_carrousel_06.jpeg';
+
+export default {
+    data () {
+      return {
+        items: [
+          {
+            title: "Sementes de Pastagem",
+            texts: [
+              "Através da parceria e cooperação com empresas do ramo, temos campos de produção na maioria das regiões produtoras de sementes no país.",
+              "Todo o sistema de produção é desenvolvido a partir de tecnologias já consagradas, que consideram, não somente a produção otimizada, mas também o respeito pleno ao meio ambiente, resultando em um sistema de produção sustentável."
+            ],
+            rightDirection: false,
+            carrousel: [
+              { src: imagCarrousel01 },
+              { src: imagCarrousel02 },
+              { src: imagCarrousel03 },
+              { src: imagCarrousel04 },
+              { src: imagCarrousel05 },
+              { src: imagCarrousel06 },
+            ]
+          },
+          {
+            title: "Frigoríficos",
+            texts: [
+              "evando sabor e qualidade para a mesa de milhares de pessoas todos os dias através dos cortes das carnes, os frigoríficos brasileiros vêm conquistando os mais variados paladares em diversos países no mundo todo.",
+              "Os padrões de fabricação das empresas parceiras são repletos de cuidado e segurança. Nosso objetivo é oferecer um produto competitivo e uma carne mais saborosa."
+            ],
+            rightDirection: true,
+            carrousel: [
+              { src: imagCarrousel01 },
+              { src: imagCarrousel02 },
+              { src: imagCarrousel03 },
+              { src: imagCarrousel04 },
+              { src: imagCarrousel05 },
+              { src: imagCarrousel06 },
+            ]
+          },
+          {
+            title: "Grãos e Milho",
+            texts: [
+              "O Brasil está entre os maiores produtores de grãos, e o milho não fica de fora nesta lista.",
+              "Vale lembrar que o País exportou 4,8 milhões de toneladas em 2004, movimentando US$ 166 milhões. Essa produção, transformada em alimentos e produtos acabados, pode agregar receita de maior valor.",
+              "O aumento do consumo humano de milho, como se vê, é um esforço que interessa a todos os elos da cadeia produtiva e aos mais de 180 milhões de brasileiros.",
+              "O milho brasileiro é seleto em qualidade e muito bem pontuado nos mercados mundiais. "
+            ],
+            rightDirection: false,
+            carrousel: [
+              { src: imagCarrousel01 },
+              { src: imagCarrousel02 },
+              { src: imagCarrousel03 },
+              { src: imagCarrousel04 },
+              { src: imagCarrousel05 },
+              { src: imagCarrousel06 },
+            ]
+          }
+        ]
+      }
+  }
+}
+</script>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,11 +1,208 @@
 <template>
-  <v-row justify="center" class="mt-4">
-    <v-col cols="12" sm="8" md="5">
-      <v-card class="logo py-4 d-flex justify-center">
-        <v-card-title>
-          CONTATO
-        </v-card-title>
-      </v-card>
+  <v-row justify="center" class="d-flex justify-center">
+    <v-col cols="md-6" class="d-flex justify-end">
+      <v-col cols="md-8" class="lighten-5 mb-6">
+        <div class="staticHero">
+          <v-row align="end" class="black--text fill-height">
+            <v-col>
+              <v-container>
+                <div
+                  class="text-h5 font-weight-bold mb-2 headline"
+                  style="color: #194f34"
+                >
+                  {{ $t('CONTACT.title') }}
+                </div>
+                <div class="text" style="font-size: 14px; font-weight: 300">
+                  {{ $t('CONTACT.subtitle') }}
+                </div>
+              </v-container>
+            </v-col>
+          </v-row>
+        </div>
+        <div class="block">
+          <v-container>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                v-model="name"
+                :rules="nameRules"
+                :label="$t('CONTACT.component.labels.name')"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="email"
+                :rules="emailRules"
+                :label="$t('CONTACT.component.labels.email')"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="subject"
+                :rules="subjectRules"
+                :label="$t('CONTACT.component.labels.subject')"
+                required
+              ></v-text-field>
+              <v-textarea
+                v-model="message"
+                :rules="messageRules"
+                :label="$t('CONTACT.component.labels.message')"
+                required
+              ></v-textarea>
+              <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="validate"
+                >{{ $t('CONTACT.component.buttons.submit') }}</v-btn
+              >
+              <v-btn color="error" class="mr-4" @click="reset">{{
+                $t('CONTACT.component.buttons.reset')
+              }}</v-btn>
+            </v-form>
+          </v-container>
+        </div>
+      </v-col>
     </v-col>
+
+    <v-col cols="md-6">
+      <v-row>
+        <v-col cols="md-8 sm-4">
+          <v-card-title class="text-h5 font-weight-bold mb-2 headline" style="color: #194f34">
+            {{ $t('CONTACT.component.our_address.title') }}
+          </v-card-title>
+          <v-row>
+            <v-col cols="12" class="pb-0">
+              <v-icon class="mr-1 pl-4" style="color: #194f34"
+                >mdi-map-marker</v-icon
+              >
+              <span class="subheading">
+                <strong style="color: #194f34"
+                  >{{ $t('CONTACT.component.our_address.address') }}:</strong
+                >
+                SRTVS QD 701 CJ L BL 01, N 38 Sala 533</span
+              >
+            </v-col>
+            <v-col cols="12" class="pb-0">
+              <v-icon class="mr-1 pl-4" style="color: #194f34">
+                mdi-phone
+              </v-icon>
+              <span class="subheading"
+                ><strong style="color: #194f34"
+                  >{{ $t('CONTACT.component.our_address.phone') }}:</strong
+                >
+                (61) 4042-6004</span
+              >
+            </v-col>
+            <v-col cols="12" class="pb-0">
+              <v-icon class="mr-1 pl-4" style="color: #194f34"> mdi-at </v-icon>
+              <span class="subheading"
+                ><strong style="color: #194f34">E-mail:</strong>
+                <a href="mailto:contato@jovemexportador.com.br">contato@jovemexportador.com.br</a></span
+              >
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="md-8 sm-4">
+          <v-card-title class="text-h5" style="color: #194f34">{{
+            $t('CONTACT.component.business_hours.title')
+          }}</v-card-title>
+
+          <v-icon class="mr-1 pl-4" style="color: #194f34">
+            mdi-calendar-clock
+          </v-icon>
+          <span class="subheading">{{
+            $t('CONTACT.component.business_hours.days_hours')
+          }}</span>
+        </v-col>
+        <v-col cols="md-8 sm-4">
+          <v-card-title class="text-h5" style="color: #194f34">{{
+            $t('CONTACT.component.get_in_touch.title')
+          }}</v-card-title>
+          <v-row>
+            <v-col cols="1">
+              <v-icon class="mr-1 pl-4" style="color: #194f34">
+                mdi-account-group
+              </v-icon>
+            </v-col>
+            <v-col cols="11" class="pl-5">
+              <span class="subheading">{{
+                $t('CONTACT.component.get_in_touch.text')
+              }}</span></v-col
+            >
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-col>
+
+    <v-snackbar
+      v-model="snackbar"
+      timeout="3000"
+    >
+      Message sent successfully!
+    </v-snackbar>
   </v-row>
 </template>
+
+<script>
+export default {
+  name: 'Contact',
+  props: {
+    nameRuleRequired: {
+      type: String,
+      default: '',
+      require: true,
+    },
+    emailRuleRequired: {
+      type: String,
+      default: '',
+      require: true,
+    },
+    emailRuleNotValid: {
+      type: String,
+      default: '',
+      require: true,
+    },
+    messageRuleRequired: {
+      type: String,
+      default: '',
+      require: true,
+    },
+    messageRuleSmall: {
+      type: String,
+      default: '',
+      require: true,
+    },
+    subjectRuleRequired: {
+      type: String,
+      default: '',
+      require: true,
+    },
+  },
+  data: (instance) => ({
+    valid: true,
+    name: '',
+    nameRules: [(v) => !!v || (instance.nameRuleRequired ?? '')],
+    email: '',
+    emailRules: [
+      (v) => !!v || (instance.emailRuleRequired ?? ''),
+      (v) => /.+@.+\..+/.test(v) || (instance.emailRuleNotValid ?? ''),
+    ],
+    message: '',
+    messageRules: [
+      (v) => !!v || (instance.messageRuleRequired ?? ''),
+      (v) => (v && v.length >= 10) || (instance.messageRuleSmall ?? ''),
+    ],
+    subject: '',
+    subjectRules: [(v) => !!v || (instance.subjectRuleRequired ?? '')],
+  }),
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.snackbar = true
+        this.reset()
+      }
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+  },
+}
+</script>
